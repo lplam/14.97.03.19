16900fd6aadd3e5e6c966a234e0936c1eb508d47
phuclam534 <lephuclamit@gmail.com>
2020-08-06 17:57:58 +0700

    Update cdsu

--- backend_cdsu/settings.py
+++ backend_cdsu/settings.py
@@ -149,6 +149,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
